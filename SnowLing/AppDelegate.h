7ba28d8a22f5278e5ebd06a67bace3fe82f75ba7
刘新乐 <liuxinle@lxldemacbook-pro.local>
2016-10-24 15:31:37 +0800

    Initial Commit

---
+++ SnowLing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowLing
+//
+//  Created by 刘新乐 on 16/10/24.
+//  Copyright © 2016年 lxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnowLing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnowLing
+//
+//  Created by 刘新乐 on 16/10/24.
+//  Copyright © 2016年 lxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
